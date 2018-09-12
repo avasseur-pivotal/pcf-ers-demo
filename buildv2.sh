a7ec2c4b242cef5175d9fd80e2eeb7bdc9ab590a
avasseur-pivotal <avasseur@pivotal.io>
2018-09-12 23:28:59 +0200

    update version, buildv2 works on mac but may break on ci & linux

--- buildv2.sh
+++ buildv2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e -x
 
-sed -e "s/3EB0FF/3EBF5C/" --in-place src/main/resources/templates/fragments/layout.html
-sed -e "s/<version>1.0/<version>2.0/" --in-place pom.xml
+sed -i -e "s/3EB0FF/3EBF5C/" src/main/resources/templates/fragments/layout.html
+sed -i -e "s/<version>1.0/<version>2.0/" pom.xml
 
-mvnw package -DskipTests
+./mvnw package -DskipTests
 
-sed -e "s/3EBF5C/3EB0FF/" --in-place src/main/resources/templates/fragments/layout.html
-sed -e "s/<version>2.0/<version>1.0/" --in-place pom.xml
+sed -i -e "s/3EBF5C/3EB0FF/" src/main/resources/templates/fragments/layout.html
+sed -i -e "s/<version>2.0/<version>1.0/" pom.xml
 
